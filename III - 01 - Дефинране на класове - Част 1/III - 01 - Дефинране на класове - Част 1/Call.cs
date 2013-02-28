1a9188218b17a9cd7c04091d524609328989c96c
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-02-28 23:05:32 +0200

    dasfaf
    sssssssssssssssss

---
+++ III - 01 - Дефинране на класове - Част 1/III - 01 - Дефинране на класове - Част 1/Call.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace III___01___Дефинране_на_класове___Част_1
+{
+    class Call
+    {
+        //<fields>
+        private DateTime data = DateTime.Now;
+        private string phoneNumber;
+        private int duration;
+
+        //</fields>
+
+        //<construcotrs>
+        public Call(string phoneNumber, int duration)
+        {
+           
+            this.phoneNumber = phoneNumber;
+            this.duration = duration;
+
+        }
+        //</constructors>
+
+        //<properties>
+        public DateTime Data
+        {
+            get
+            {
+                return this.data;
+            }
+            
+        }
+        public string PhoneNumber
+        {
+            get
+            {
+                return this.phoneNumber;
+            }
+
+        }
+        public int Duration
+        {
+            get
+            {
+                return this.duration;
+            }
+
+        }
+        //</properties>
+
+        
+    }
+}
