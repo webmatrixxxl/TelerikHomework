f2a7bf50a37be4b7ad4c58cf0a46ddcea860d9ba
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-02-28 23:01:11 +0200

    update

---
+++ III - 01 - Дефинране на класове - Част 1/III - 01 - Дефинране на класове - Част 1/GSMCallHistoryTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace III___01___Дефинране_на_класове___Част_1
+{
+    class GSMCallHistoryTest
+    {
+        GSM BlackBerry = new GSM("Bold 9900", "Rim BlackBarry", 550.99M, "Kiril");
+        Call FirstCall = new Call("0878756132", 3600);
+            Call SecondCall = new Call("087888888", 3600);
+
+            BlackBerry.AddCall(FirstCall.PhoneNumber, FirstCall.Duration);
+        
+    }
+}
