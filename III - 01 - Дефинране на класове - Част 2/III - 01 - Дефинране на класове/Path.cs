e92c4e263a0d32c23d4cd263ac6c997c04df28e2
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-03-05 01:00:22 +0200

    Revert "Go go"
    This reverts commit 15325082b136a18cf55eb3829557827a02098fcf.

--- III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Path.cs
+++ III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Path.cs
@@ -8,26 +8,17 @@ namespace III___01___Дефинране_на_класове
 {
     class Path
     {
-        private List<Point3D> path = new List<Point3D>();
-
+        private List<Point3D> path;
         public Path(params Point3D[] points)
         {
 
         }
-
-        public List<Point3D> PathList
-        {
-            get
-            {
-                return this.path;
-            }
-        }
-
         public void AddPoint(params Point3D[] point)
         {
             foreach (var item in point)
             {
                 path.Add(item);
+                
             }
 
         }
