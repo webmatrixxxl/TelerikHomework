598942fee83e7ceaa06b46138d6d4414c7d55b97
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-03-04 18:33:53 +0200

    new homework start

---
+++ III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Point3D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace III___01___Дефинране_на_класове
+{
+    struct Point3D
+    {
+        //<fields>
+
+        private int X { get; set; }
+        private int Y { get; set; }
+        private int Z { get; set; }
+
+        static readonly Point3D O;
+        //</fields>
+
+        //<constructor>
+        static Point3D()
+        {
+            O = new Point3D(0, 0, 0);
+        }
+        public Point3D(int X, int Y, int Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+        //</constructor>
+
+        //<property>
+        public static Point3D PointO
+        {
+            get
+            {
+                return O;
+            }
+        }
+        //</property>
+    }
+}
---
+++ III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace III___01___Дефинране_на_класове
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Point3D.PointO);
+        }
+    }
+}
