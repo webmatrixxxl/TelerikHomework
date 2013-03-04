23fbaa1a33566e2124a3cb195611affc3ac9b622
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-03-05 01:08:39 +0200

    ne se kosi

--- III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Path.cs
+++ III - 01 - Дефинране на класове - Част 2/III - 01 - Дефинране на класове/Path.cs
@@ -8,10 +8,17 @@ namespace III___01___Дефинране_на_класове
 {
     class Path
     {
-        private List<Point3D> path;
+        private List<Point3D> path = new List<Point3D>();
         public Path(params Point3D[] points)
         {
 
+        }
+        public List<Point3D> PathList
+        {
+            get
+            {
+                return this.path;
+            }
         }
         public void AddPoint(params Point3D[] point)
         {
