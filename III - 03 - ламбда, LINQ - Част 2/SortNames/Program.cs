c09293cdebdc9ecc5719781a65baeff63d7e6126
Кирил Даскалов <webmatrixxxl@gmail.com>
2013-03-12 19:00:34 +0200

    LAmbda LINQ

---
+++ III - 03 - ламбда, LINQ - Част 2/SortNames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            students Kiro = new students("Kiril", "Daskalov");
+            students Go6o = new students("Go6ko", "Petrov");
+            students Rami = new students("Rami", "Popov");
+            
+            List<students> all = new List<students>();
+
+            all.Add(Kiro);
+            all.Add(Go6o);
+            all.Add(Rami);
+            
+            var newList  = from names  in all orderby students.FirstName;
+
+            
+        }
+    }
+}
---
+++ III - 03 - ламбда, LINQ - Част 2/SortNames/students.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNames
+{
+    class students
+    {
+        private string firstName;
+        private string lastName;
+
+        public students(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                this.lastName = value;
+            }
+        }
+
+    }
+}
