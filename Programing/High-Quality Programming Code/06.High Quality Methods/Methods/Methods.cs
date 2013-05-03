e44cf52693d50b6e299f0f81e7a5ef555d1ef7c4
Kiril Daskalov <webmatrixxxl@gmail.com>
2013-05-03 12:05:48 -0700

    HightQualityMetods

---
+++ Programing/High-Quality Programming Code/06.High Quality Methods/Methods/Methods.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArgumentException("Sides should be positive.");
+            }
+
+            double halfPerimeter = (a + b + c) / 2;
+            double area = Math.Sqrt(halfPerimeter *
+                (halfPerimeter - a) *
+                (halfPerimeter - b) *
+                (halfPerimeter - c));
+
+            return area;
+        }
+
+        static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                default: throw new ArgumentException("Invalid number!");
+            }
+        }
+
+        static int FindMaxNumber(params int[] elements)
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException("Elements cant be null");
+            }
+            if (elements.Length == 0)
+            {
+                throw new ArgumentException("Array must have at least one element!");
+            }
+
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] > elements[0])
+                {
+                    maxNumber = elements[i];
+                }
+            }
+
+            return maxNumber;
+        }
+
+        static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+            if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+            if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+        }
+
+
+        static double CalcDistance(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isVertical = (y1 == y2);
+            isHorizontal = (x1 == x2);
+            double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMaxNumber(5, -1, 3, 2, 14, 2, 3));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool isHorizontal;
+            bool isVertical;
+
+            Console.WriteLine(CalcDistance(3, -1, 3, 2.5, out isHorizontal, out isVertical));
+            Console.WriteLine("Horizontal? " + isHorizontal);
+            Console.WriteLine("Vertical? " + isVertical);
+
+
+            Student peter = new Student("Peter", "Ivanov", "17.03.1992", "From Sofia");
+            Student stella = new Student("Stella", "Markova", "03.11.1993", "From Vidin, gamer, high results");
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
---
+++ Programing/High-Quality Programming Code/06.High Quality Methods/Methods/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public string City { get; set; }
+        public string Hobby { get; set; }
+
+        public Student(string firstName, string lastName, string birthDate, string city = null, string hobby = null)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.City = city;
+            this.Hobby = hobby;
+
+            DateTime parsedDataTime;
+            if (!DateTime.TryParse(birthDate, out parsedDataTime))
+            {
+                throw new ArgumentException("Date format is incorect !");
+            }
+        }
+        public bool IsOlderThan(Student student)
+        {
+
+            bool isOlder = false;
+            DateTime firstStudent = this.BirthDate;
+            DateTime secondStudent = student.BirthDate;
+
+            if (DateTime.Compare(firstStudent, secondStudent) < 0)
+            {
+                isOlder = true;
+            }
+
+            return isOlder;
+        }
+    }
+}
