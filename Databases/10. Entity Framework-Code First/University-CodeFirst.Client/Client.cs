4fa95090dabaf51513ed645f8070ce1e16a6e972
Kiril Daskalov <webmatrixxxl@gmail.com>
2013-07-21 20:25:53 +0300

    fix naming and comments

--- Databases/10. Entity Framework-Code First/University-CodeFirst.Client/Client.cs
+++ Databases/10. Entity Framework-Code First/University-CodeFirst.Client/Client.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿//  Task 01. Using c0de first approach, create database for student system with the following tables:
+//  Students (with Id, Name, Number, etc.)
+//  Courses (Name, Description, Materials, etc.)
+//  StudentsInCourses (many-to-many relationship)
+//  Homework (one-to-many relationship with students and courses), fields: Content, TimeSent
+//  Annotate the data models with the appropriate attributes and enable code first migrations
+
+//  Task 02. Write a console application that uses the data
+
+//  Task 03 .Seed the data with random values
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -10,20 +23,19 @@ using University_CodeFirst.Models;
 
 namespace University_CodeFirst.Client
 {
-    internal class Program
+    internal class Client
     {
         static void Main(string[] args)
         {
-
+            // All tasks: 
             var db = new University_CodeFirstContext();
-
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<University_CodeFirstContext, Configuration>());
 
-
-            var course = new Course {
+            var course = new Course
+            {
                 Description = "Шопско хоро",
                 Materials = "Video of playing",
-                Name="Хоро Курс" 
+                Name = "Хоро Курс"
             };
 
             db.Course.Add(course);
@@ -33,18 +45,15 @@ namespace University_CodeFirst.Client
             student.Name = "Димитър божков";
             student.Number = 123;
             student.Courses.Add(course);
-            student.Homeworks.Add(new Homework {
+            student.Homeworks.Add(new Homework
+            {
                 Content = "Дайкино хоро",
-                TimeSent = new DateTime(2012,02,01),
-                DeadLine = new DateTime(2013,01,15) 
+                TimeSent = new DateTime(2012, 02, 01),
+                DeadLine = new DateTime(2013, 01, 15)
             });
 
             db.Student.Add(student);
             db.SaveChanges();
-
-
         }
-
-
     }
 }
